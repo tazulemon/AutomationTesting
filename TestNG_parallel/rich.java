56f682f92bd30bc6d40d6ade6b6ffc62602ace7d
tazulemon <57292743+tazulemon@users.noreply.github.com>
2019-11-04 19:43:21 -0500

    Update rich.java

--- TestNG_parallel/rich.java
+++ TestNG_parallel/rich.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.testng.annotations.AfterClass;
-
+//This is tazul
 public class rich {
   
 	public WebDriver driver;
